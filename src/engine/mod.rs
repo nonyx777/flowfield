eb5a822ccbdc56b4e560b84b1e6e135655566586
nonyx777 <nahomretebo@gmail.com>
2023-06-25 12:30:55 +0300

    flow field grid vector adjustment implemented

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -4,18 +4,22 @@ use sfml::window::Event;
 use sfml::system::*;
 
 use crate::entities::r#box;
+use crate::entities::ball;
+use crate::operations;
 
-pub struct Engine{
+pub struct Engine<'a>{
     window: RenderWindow,
     //grid related variables
     grid: [[Vector2f; 40];40],
     size: u32,
     row: u32,
-    col: u32
+    col: u32,
+    ball: ball::Ball<'a>,
+    mouse_position_view: Vector2f
 }
 
-impl Engine{
-    pub fn new(width: u32, height: u32) -> Engine{
+impl Engine<'_>{
+    pub fn new(width: u32, height: u32) -> Engine<'static>{
         let mut window = RenderWindow::new(
             (width, height),
             "Window",
@@ -30,12 +34,19 @@ impl Engine{
         let col: u32 = window.size().x/size;
         let mut grid: [[Vector2f; 40];40] = [[Vector2f::default();40];40];
         Self::gridLayout(&mut grid, size);
+
+        //instantiating ball
+        let ball: ball::Ball = ball::Ball::new(1_f32);
+        //initializing mouse position variable
+        let mouse_position_view: Vector2f = Vector2f::default();
         Engine { 
             window,
             grid,
             size,
             row,
-            col
+            col,
+            ball,
+            mouse_position_view
         }
     }
 
@@ -54,16 +65,17 @@ impl Engine{
 
     pub fn update(&mut self) {
         self.poll_event();
+        //getting mouse position
+        self.mouse_position_view = self.window.map_pixel_to_coords(self.window.mouse_position(), self.window.view());
 
-        for i in 0..40{
-            for j in 0..40{
-                println!("{}, {}", self.grid[i][j].y, self.grid[i][j].x);
-            }
-        }
+        //.....
+        Self::gridAdjustVector(&mut self.grid, &self.mouse_position_view);
+        self.ball.update(&self.grid);
     }
 
     pub fn render(&mut self) {
         self.window.clear(Color::BLACK);
+        self.ball.render(&mut self.window);
         self.window.display();
     }
 
@@ -71,7 +83,17 @@ impl Engine{
     pub fn gridLayout(grid: &mut [[Vector2f; 40];40], size: u32){
         for i in 0..40{
             for j in 0..40{
-                grid[i][j] = Vector2f::new(j as f32 * size as f32, i as f32* size as f32);
+                grid[i][j] = Vector2f::new(j as f32 * size as f32, i as f32 * size as f32);
+            }
+        }
+    }
+
+    pub fn gridAdjustVector(grid: &mut[[Vector2f; 40];40], vector: &Vector2f){
+        for i in 0..40{
+            for j in 0..40{
+                //grid[i][j] = *vector - grid[i][j];
+                grid[i][j] = Vector2f::new(0_f32, -1_f32);
+                println!("{}, {}", vector.x, vector.y);
             }
         }
     }
--- src/entities/ball/mod.rs
+++ src/entities/ball/mod.rs
@@ -1,5 +1,6 @@
 #[allow(non_snake_case)]
 
+use std::f64;
 use std::time;
 use sfml::graphics::*;
 use sfml::system::*;
@@ -12,6 +13,8 @@ pub struct Ball<'a>{
     radius: f32,
     velocity: Vector2f,
     acceleration: Vector2f,
+    max_speed: f32,
+    max_force: f32
 }
 
 impl Ball<'_>{
@@ -19,21 +22,47 @@ impl Ball<'_>{
         let mut property = CircleShape::new(radius, 30_usize);
         property.set_fill_color(Color::WHITE);
         property.set_origin((radius, radius));
-        property.set_position(Vector2f::new(800_f32/2_f32, 600_f32/2_f32));
+        property.set_position(Vector2f::new(800_f32/2_f32, 800_f32/2_f32));
+
+        let max_speed: f32 = 2_f32;
+        let max_force: f32 = 0.5_f32;
+
         Ball {
             property,
             radius,
             velocity: Vector2f::new(1_f32, 1_f32),
             acceleration: Vector2f::new(0_f32, 0_f32),
+            max_speed,
+            max_force
         }
     }
 
     //updater and displayer
-    pub fn update(&mut self, vector: &Vector2f){
-        //....
+    pub fn update(&mut self, grid: &[[Vector2f; 40];40]){
+        self.seek(grid);
     }
     pub fn render(&mut self, target: &mut dyn RenderTarget){
         target.draw(&self.property);
+    }
+
+    pub fn seek(&mut self, grid: &[[Vector2f;40];40]){
+        let x: usize = ((self.getPosition().x/20_f32).floor()) as usize;
+        let y: usize = ((self.getPosition().y/20_f32).floor()) as usize;
+        
+
+        let mut desired: Vector2f = grid[x][y];
+        //desired = operations::normalize(desired);
+        //desired *= self.max_speed;
+
+        //let steer: Vector2f = desired - self.getVelocity();
+
+        //integration
+        self.setAcceleration(Vector2f::default());
+        self.setVelocity(desired);
+        self.property.move_(self.getVelocity() * 0.5);
+
+        //revert acceleration
+        self.setAcceleration(Vector2f::default());
     }
 
     //accessors and mutators
